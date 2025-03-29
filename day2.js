1612a1f8c744b7ff66041a8e010f0942c91157a2
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-03-29 18:20:28 +0530

    first commit

---
+++ day2.js
@@ -0,0 +1,19 @@
+// var a = 12
+// var b = 16
+// console.log(" the sum of a and the sum of b is ", (a + b));
+function lakhan() {
+    
+    // var a = 5
+    // var b = 9
+    // console.log(" the sum of " + a + " and the sum of " + b + " is " + (a + b));
+    console.log("lakhan");
+}
+lakhan() // calling the funtion
+function smarty() {
+    console.log("smarty");
+    
+}
+smarty() // calling the function
+// calling ke upar depend krta hai ki kon pehle call hua
+// aero function is mostly used in company 
+  
