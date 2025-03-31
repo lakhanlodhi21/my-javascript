cfb8fc65767c1986c47c6a4e00e76689f9873853
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-03-31 18:28:42 +0530

    update

---
+++ BOM.js
@@ -0,0 +1,21 @@
+// Windows method 
+
+function smart() {
+
+    // window.alert("Hello i am lakhan ") // alert
+    // window.confirm("are u ok?")
+    // let confirmans = window.confirm("are u ok?") // confirm
+    // console.log(confirmans);
+    // let a = window.prompt(" What is your name") // promt
+    // window.alert(a)
+    let ans = confirm("do you like js")
+    if (ans) {
+        
+        alert("thanks")
+    }
+    else {
+        alert(" jaa yrr bhai ")
+    }
+
+}
+smart()
