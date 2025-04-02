8ca8f98e938de2313b1336c859e2823ca823bb89
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-04-02 18:32:21 +0530

    updated

--- string.js
+++ string.js
@@ -1,17 +1,108 @@
 
-    let a = "This is string 1" // aishee koi  bhii cheez jo double code ya single code ke andar likhi hoti hai string hoti hai
-    // string methods -->>>
-    
-    // length->> returns the length of the string
-    // slice()-->> returns the elements between the two indexex of a string
-    // concade -->> 2 string ko jod ke nayi string bana ke deta hai
-    // substring()-->> returns the  elements 
-    // replace -->> text ko replace krne ke liye use kiya jata hai 
-    // toUppercase -->> sabhi string ko upper case me convert krta hai
-    // toLowercase -->> sabhi string ko lower case me convert krta hai
-    // replaceall -->> matching matching ke sabhi words ko replace krta hai 
-    // charAT -->> returns character at a specific index index number lega
-    // split -->> string ko array me convert kr deti hai
-    // index off -->> index value return krega
+let a = "This is string 1"
+let b = "This is string 2"
+// aishee koi  bhii cheez jo double code ya single code ke andar likhi hoti hai string hoti hai
+// string methods -->>>
 
-    console.log(a.length)
+// length->> returns the length of the string
+// slice()-->> returns the elements between the two indexex of a string
+// concat -->> 2 string ko jod ke nayi string bana ke deta hai concat and + same hai dono jodne ka kaaam krte hai
+// substring()-->> returns the  elements
+// replace -->> text ko replace krne ke liye use kiya jata hai
+// toUppercase -->> sabhi string ko upper case me convert krta hai
+// toLowercase -->> sabhi string ko lower case me convert krta hai
+// replaceall -->> matching matching ke sabhi words ko replace krta hai
+// charAT -->> returns character at a specific index
+// split -->> string ko array me convert kr deti hai
+// index off -->> index value return krega
+// charcodeat -->> ye asqi value batata hai
+
+
+ console.log(a.length)
+ document.write(a.concat(b)) // 2 string ko jod ke nayi string return krta hai 
+
+document.write("<br>" +a.toUpperCase())
+document.write("<br>" + a.toLocaleLowerCase())
+console.log(b.slice(6,10)) // 2 string ke beech me jo likha hota hai bo return krta hai  
+
+document.write(a.charAt(5))
+document.write(a.charCodeAt(1))
+console.log(a.trim(8, 10)) // remove extra space
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
