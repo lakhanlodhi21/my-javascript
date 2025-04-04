800ecf65436f0ad6e18e360dda7454bb828926b5
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-04-04 18:31:19 +0530

    update

---
+++ array.js
@@ -0,0 +1,25 @@
+// colllection of similar and diffrent datatypes
+let fruits = ["apple", "mango", "banana", "grapes", "watermelon"] // ye hai array
+let animal = ["kutta", "billi", "ghoda ", "gadha ", "saand"]
+let sabji = ["aalu","bhate","bhindi","shimla"]
+console.log(fruits.concat(animal,sabji))
+// non primitive datatypes = array
+// array methods
+// lenght -->> length return krega
+// pop -->> last se remove krega
+// push -->> aakhiri se insert krega element
+// shift -->> aage se element hatayega
+// unshift aage se element lagayega
+
+// console.log(fruits[2]);
+// console.log(fruits.length)
+// fruits.pop() last se element remove krega
+// console.log(fruits)
+// fruits.push("daaru") last se element add krega
+// console.log(fruits)
+// fruits.shift() aage se element remove krega
+// console.log(fruits) aage se element lagayega
+// splice -->> beech me tukda add kregii
+// slice -->> beech me se tukda nikal ke degi
+//  fruits.unshift()
+
--- loops.js
+++ loops.js
@@ -1,7 +1,18 @@
-let game = 25;
-let user = prompt("guess the game number");
-while (user!=game) {
-   user = prompt("wrong number")
-}
-console.log("congratulations")
+// let game = 25;
+// let user = prompt("guess the game number");
+// while (user!=game) {
+//    user = prompt("wrong number")
+// }
+// console.log("congratulations")
+
+let name = "lakhan";
+let age = '21';
+
+let a = `hello a am ${name} and my age is ${age}`
+
+document.write(a)
+
+// 1st use supports single quotes (``) and doubble ("")
+// 2nd use supports multi-line string
+// 3rd use supports html tags
 
