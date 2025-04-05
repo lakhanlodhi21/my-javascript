797ca1ae229827b7cbe3af68994bae007baf2cc3
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-04-05 13:17:40 +0530

    updated

--- array.js
+++ array.js
@@ -1,8 +1,8 @@
 // colllection of similar and diffrent datatypes
-let fruits = ["apple", "mango", "banana", "grapes", "watermelon"] // ye hai array
-let animal = ["kutta", "billi", "ghoda ", "gadha ", "saand"]
-let sabji = ["aalu","bhate","bhindi","shimla"]
-console.log(fruits.concat(animal,sabji))
+// let fruits = ["apple", "mango", "banana", "grapes", "watermelon"] // ye hai array
+// let animal = ["kutta", "billi", "ghoda ", "gadha ", "saand"]
+// let sabji = ["aalu","bhate","bhindi","shimla"]
+// console.log(fruits.concat(animal,sabji))
 // non primitive datatypes = array
 // array methods
 // lenght -->> length return krega
@@ -19,7 +19,17 @@ console.log(fruits.concat(animal,sabji))
 // console.log(fruits)
 // fruits.shift() aage se element remove krega
 // console.log(fruits) aage se element lagayega
-// splice -->> beech me tukda add kregii
+// splice -->> beech me tukda add kregii remove kregi and replace bhi kregi
 // slice -->> beech me se tukda nikal ke degi
 //  fruits.unshift()
+
+// fruits.splice(1,2,101,102) //  beech me add bhi kregi and replace bhi kregi and remove bhi kregii
+
+// let com = ["boomberg", "microsoft", "uber", "gooogle", "ibm", "netflix"]
+// // com.shift()
+// com.splice(2, 1, "ola")
+// com.push("amazon")
+
+
+
 
---
+++ function.js
@@ -0,0 +1,35 @@
+// function is a peace of code or block of code that perform a some specific task
+// function hame redundancy(repeat hone se) se bachate hai
+
+
+// function sabji() {
+//     //parameter -->> input
+//     console.log("sabji chahiye kya aapko")
+//     console.log("free ki sabji hai")
+
+// }
+// sabji() // call krna bhi bol sakte hai ya invoke krna bhii bol sakte hai means bulana
+//function call ke andar parameter ko call krte hai jo argument kehlata hai
+
+// function vowels(str) {
+//     let count = 0;
+//     for (const char of str)
+//     if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//         count++;
+//     }
+    
+//         console.log(count);
+// }
+
+
+// const val = (str) =>
+// {
+//     let count = 0;
+//      for (const char of str)
+//      if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//          count++;
+//      }
+    
+//          console.log(count);
+// }
+
