d907e7faca3b5de1d54d40cde91356793425da84
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-04-10 14:01:53 +0530

    update

--- practice.js
+++ practice.js
@@ -189,3 +189,18 @@
 // let photo = () => {
 //     let photo1 = document.querySelector("#change").src = "./pandey.smartyy.jpg";
 // }
+
+// }
+let fun2 = () => {
+    let hy = document.querySelector("#box1").src = "my2.jpg"
+
+}
+let fun3 = () => {
+    let hy = document.querySelector("#box1").src = "my3.jpg";
+
+}
+let fun4 = () => {
+    let hy = document.querySelector("#box1").src = "img3.jpg";
+
+}
+
