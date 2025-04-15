5f33fb4b0d796758bcf659f4b5342daa9903aae7
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-04-15 18:10:03 +0530

    updated

---
+++ object.js
@@ -0,0 +1,21 @@
+// let arr = ["name", 25, "bhopal"]
+// let obj = {
+//     Name: "lakhan",
+//     age: 25,
+//     city: "Bhopal"
+   
+// }
+// console.log(obj) // way to print full object
+// console.log(obj.age) // way to print key value of object
+// obj.college = "sage university"
+// obj.age = 26 // way to update
+// // delete obj.Name // key value ko delete krne ka tarika
+// console.log(obj)
+// D-struchring and spread operator // interview question
+//  const  cities = {name:"lakhan",Age: 21,city: "bhopal"}
+// const { name,Age,city } = cities;
+// console.log(city) --------->>>>>>>>>// d-strucher krne ka way
+//----------------------->>>>>>>>>>>>>>>>>>>>> WHy use destrucher
+// Makes the code cleaner and easier to read.
+// Allows Quick access to object properties.
+
